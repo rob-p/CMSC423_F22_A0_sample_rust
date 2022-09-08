04e2ffea643e5fb6f5d0cfaba2e6c7de5cfdd107
rob-p <rob.patro@gmail.com>
2022-09-08 15:34:28 -0400

    update with autograder path

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-cargo build --release
+# Note; this is the path that should be used to 
+# execute cargo on the autograder.  Change this 
+# appropriately to run locally
+CARGO_CMD="/root/.cargo/bin/cargo"
+
+$CARGO_CMD build --release
 mv ./target/release/fasta_stats fasta_stats
